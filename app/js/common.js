9d023d862f6412cf528c43d96e9b43654757ff06
Sergey <kalinin.ss@hotmail.com>
2016-08-23 18:52:00 +0700

    All pages
    Все страницы, с попапами и валидностью.
    Осталось проверить кроссбраузерность и сделать универсальную шапку и
    футер.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
+	// закрытие всплывающих окон
+	$(".open-popup").click(function (i) {
+		$(".popup_bg").show();
+		elementClick = $(this).attr("href")
+		$(elementClick).show(300);
+		return false;
+	});
+	$(".i-close").click(function(){
+		$(this).parent().hide(300);
+		$(".popup_bg").hide();
+	});
+	$(".popup_bg").click(function() {
+		$(this).hide();
+		$(".popup").hide(300);		
+	});
+
+	// вкладки в профиле
+  $(".tabs").lightTabs();
+
+	// маска для телефонов
+	$("#d").mask("+7 (999) 999-9999");
+
+	// навигация
 	$(".nav_open-btn").click(function(e){
 		$(this).parent().find("ul.nav").slideToggle(200);
 		e.preventDefault();
